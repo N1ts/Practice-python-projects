c3cf0fcf27f922fb0fe163dbcbf60873130062ed
Nitesh <sharma.nitesh430@gmail.com>
2024-07-29 00:15:00 +0530

    Implement the Add button

--- Practice/Todos/todo_list_using_gui_freesimplegui.py
+++ Practice/Todos/todo_list_using_gui_freesimplegui.py
@@ -2,9 +2,20 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_text = sg.InputText(tooltip="Enter todo")
+input_text = sg.InputText(tooltip="Enter todo", key="todo")
 button = sg.Button("Add")
 
-window = sg.Window('My Todo App', layout=[[label], [input_text,button]])
-window.read()
+window = sg.Window('My Todo App', layout=[[label], [input_text,button]], font=('Helvetica', 16))
+
+while True:
+    events, values = window.read()
+    match events:
+        case "Add":
+            todos = functions.read_file()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_file(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
