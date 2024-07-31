d0234cbf4763b0e408eb945eb7d42cf940089574
Nitesh <sharma.nitesh430@gmail.com>
2024-07-31 22:48:52 +0530

    Implement Delete, Exit button

--- Practice/Todos/todo_list_using_gui_freesimplegui.py
+++ Practice/Todos/todo_list_using_gui_freesimplegui.py
@@ -8,11 +8,14 @@ add_button = sg.Button("Add")
 text_area = sg.Listbox(size=(40,10), values=functions.read_file(), key="todos",
                        enable_events=True)
 edit_button = sg.Button("Edit")
+delete_button = sg.Button("Delete")
+exit_button = sg.Button("Exit")
 
 #display window with UI elements
 window = sg.Window('My Todo App', layout=[[label],
                     [input_text,add_button],
-                    [text_area, edit_button]],
+                    [text_area, edit_button, delete_button],
+                    [exit_button]],
                    font=('Helvetica', 16))
 
 # while loop to keep showing the window until we close manually
@@ -34,14 +37,23 @@ while True:
 
             todos = functions.read_file()
             index = todos.index(todo_to_edit)
-            print(index)
+            # print(index)
             todos[index] = new_todo
             print(todos[index])
-            print(todos)
+            # print(todos)
             functions.write_file(todos)
             window['todos'].update(values=todos)
+        case "Delete":
+            todo_to_delete = values['todos'][0]
+            todos = functions.read_file()
+            todos.remove(todo_to_delete)
+            functions.write_file(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case "todos":
             window['todo'].update(value=values['todos'][0])
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
 
